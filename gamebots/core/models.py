056154dd3032504ba725717845370b0afd43c09f
Dmitry G <sdprecovery@gmail.com>
2022-11-07 19:27:51 +0300

    Update core: test_models, factories; Product alter field slug

--- gamebots/core/models.py
+++ gamebots/core/models.py
@@ -5,6 +5,8 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from model_utils.models import TimeStampedModel
 
+from gamebots.core.utils import get_unique_slug
+
 
 class Product(TimeStampedModel):
     # choices
@@ -16,7 +18,7 @@ class Product(TimeStampedModel):
     # fields
     uuid = models.UUIDField(db_index=True, default=uuid_lib.uuid4, editable=False)
     title = models.CharField(_("Title"), max_length=55)
-    slug = models.CharField(_("Slug"), max_length=55, default="")
+    slug = models.SlugField(_("Slug"), unique=True, max_length=55)
     description = models.TextField(_("Description"), blank=True, default="")
     price = models.FloatField(_("Price"), default=0.0)
     currency = models.CharField(
@@ -32,6 +34,11 @@ class Product(TimeStampedModel):
 
     def __str__(self):
         return f"{self.title} : {self.price}"
+
+    def save(self, *args, **kwargs):
+        if not self.slug or self.title != self.tracker.previous("title"):
+            self.slug = get_unique_slug(Product, self.title)
+        return super().save(*args, **kwargs)
 
 
 class Order(TimeStampedModel):
--- gamebots/core/tests/test_models.py
+++ gamebots/core/tests/test_models.py
@@ -1,21 +1,59 @@
 from django.test import TestCase
 
+from ..models import Order, Product
 from .factories import OrderFactory, ProductFactory
 
 
 class TestProduct(TestCase):
     def setUp(self):
-        pass
+        self.batch_size = 10
+        self.objects = ProductFactory.create_batch(size=self.batch_size)
 
-    def test_test(self):
-        product = ProductFactory()
-        print(product)
+    def test_create(self):
+        assert len(self.objects) == self.batch_size
+
+    def test_update(self):
+        new_title = "new title"
+        for obj in self.objects:
+            obj.title = new_title
+            obj.save()
+        for obj in self.objects:
+            assert obj.title == new_title
+
+    def test_delete(self):
+        for obj in self.objects:
+            obj.delete()
+        qs = Product.objects.all()
+        assert not len(qs)
+
+    def test_fields(self):
+        for obj in self.objects:
+            assert obj.uuid
 
 
 class TestOrder(TestCase):
     def setUp(self):
-        pass
+        self.batch_size = 10
+        self.objects = OrderFactory.create_batch(size=self.batch_size)
 
-    def test_test(self):
-        order = OrderFactory()
-        print(order)
+    def test_create(self):
+        assert len(self.objects) == self.batch_size
+
+    def test_update(self):
+        new_title = "new title"
+        for obj in self.objects:
+            obj.title = new_title
+            obj.save()
+        for obj in self.objects:
+            assert obj.title == new_title
+
+    def test_delete(self):
+        for obj in self.objects:
+            obj.delete()
+        qs = Order.objects.all()
+        assert not len(qs)
+
+    def test_fields(self):
+        for obj in self.objects:
+            assert obj.uuid
+            assert obj.user.username
