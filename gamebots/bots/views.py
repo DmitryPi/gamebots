71ac675cf46548b743a2f81716f1ad3e1e6fd2c1
Dmitry G <sdprecovery@gmail.com>
2022-11-08 17:28:13 +0300

    update views: BotDetailView

--- gamebots/bots/views.py
+++ gamebots/bots/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render  # noqa skip
+from django.shortcuts import get_object_or_404, render  # noqa skip
 from django.views.generic import DetailView, ListView
 
 from .models import Bot
@@ -6,9 +6,19 @@ from .models import Bot
 
 class BotListView(ListView):
     model = Bot
-    template_name = "pages/home.html"
     queryset = Bot.objects.all()
+    template_name = "pages/home.html"
 
 
 class BotDetailView(DetailView):
     model = Bot
+
+    def get_object(self, *args, **kwargs):
+        obj = get_object_or_404(
+            Bot.objects.prefetch_related(), slug=self.slug_url_kwarg
+        )
+        return obj
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        return context
